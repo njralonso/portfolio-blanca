5143c1a611409d8e9c59c0fd940a009cfee7bd7c
njralonso <njralonso@outlook.com>
2025-09-07 05:31:32 +0200

    Se ha hecho un rebrand completo, se eliminó todo lo anterior, se creó un layout, componentes, agregado imágenes, y demás

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
-import icon from 'astro-icon';
-
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), icon()]
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
